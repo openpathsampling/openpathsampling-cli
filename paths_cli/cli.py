a6584aeb591b5b8feeeeb7343600c9246e60df9a
David W.H. Swenson <dwhs@hyperblazer.net>
2020-02-05 15:29:19 +0100

    Ensure that plugins preserve directory order

--- paths_cli/cli.py
+++ paths_cli/cli.py
@@ -1,33 +1,38 @@
+"""OpenPathSampling command line interface
+
+This contains the "main" class/functions for running the OPS CLI.
+"""
 # builds off the example of MultiCommand in click's docs
 import collections
-import click
 import os
+
+import click
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-plugin_folder = os.path.join(os.path.dirname(__file__), 'commands')
-
-_possible_plugin_folders = list(set([
+_POSSIBLE_PLUGIN_FOLDERS = [
     os.path.join(os.path.dirname(__file__), 'commands'),
     os.path.join(click.get_app_dir("OpenPathSampling"), 'cli-plugins'),
     os.path.join(click.get_app_dir("OpenPathSampling", force_posix=True),
                  'cli-plugins'),
-]))
+]
 
 OPSPlugin = collections.namedtuple("OPSPlugin",
                                    ['name', 'filename', 'func', 'section'])
 
+
 class OpenPathSamplingCLI(click.MultiCommand):
+    """Main class for the command line interface
+
+    Most of the logic here is about handling the plugin infrastructure.
+    """
     def __init__(self, *args, **kwargs):
         # the logic here is all about loading the plugins
-        plugin_folders = [
-            os.path.join(os.path.dirname(__file__), 'commands'),
-            os.path.join(click.get_app_dir("OpenPathSampling",
-                                           force_posix=True),
-                         'cli-plugins')
-        ]
-        self.plugin_folders = [f for f in _possible_plugin_folders
-                               if os.path.exists(f)]
+        self.plugin_folders = []
+        for folder in _POSSIBLE_PLUGIN_FOLDERS:
+            if folder not in self.plugin_folders and os.path.exists(folder):
+                self.plugin_folders.append(folder)
+
         plugin_files = self._list_plugin_files(self.plugin_folders)
         self.plugins = self._load_plugin_files(plugin_files)
 
@@ -65,7 +70,7 @@ class OpenPathSamplingCLI(click.MultiCommand):
     def _load_plugin_files(self, plugin_files):
         plugins = []
         for full_name in plugin_files:
-            head, filename = os.path.split(full_name)
+            _, filename = os.path.split(full_name)
             command_name = self._filename_to_command_name(filename)
             func, section = self._load_plugin(full_name)
             plugins.append(OPSPlugin(name=command_name, filename=full_name,
@@ -95,7 +100,7 @@ class OpenPathSamplingCLI(click.MultiCommand):
                     formatter.write_dl(rows)
 
 
-main_help="""
+_MAIN_HELP = """
 OpenPathSampling is a Python library for path sampling simulations. This
 command line tool facilitates common tasks when working with
 OpenPathSampling. To use it, use one of the subcommands below. For example,
@@ -105,13 +110,15 @@ tool with:
     openpathsampling strip-snapshots --help
 """
 
+
 def main():  # no-cov
     cli = OpenPathSamplingCLI(
         name="openpathsampling",
-        help=main_help,
+        help=_MAIN_HELP,
         context_settings=CONTEXT_SETTINGS
     )
     cli()
+
 
 if __name__ == '__main__':  # no-cov
     main()
