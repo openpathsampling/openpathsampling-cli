0b33b7938520138549892d6ab7989694c23ccb7e
David W.H. Swenson <dwhs@hyperblazer.net>
2021-07-24 10:02:34 -0400

    no-cov on joke

--- paths_cli/wizard/joke.py
+++ paths_cli/wizard/joke.py
@@ -16,26 +16,26 @@ _MISC = [
     "It would also be a good name for a death metal band.",
 ]
 
-def _joke1(name, obj_type):
+def _joke1(name, obj_type):  # no-cov
     return (f"I probably would have named it something like "
             f"'{random.choice(_NAMES)}'.")
 
-def _joke2(name, obj_type):
+def _joke2(name, obj_type):  # no-cov
     thing = random.choice(_THINGS)
     joke = (f"I had {a_an(thing)} {thing} named '{name}' "
             f"when I was young.")
     return joke
 
-def _joke3(name, obj_type):
+def _joke3(name, obj_type):  # no-cov
     return (f"I wanted to name my {random.choice(_SPAWN)} '{name}', but my "
             f"wife wouldn't let me.")
 
-def _joke4(name, obj_type):
+def _joke4(name, obj_type):  # no-cov
     a_an_thing = a_an(obj_type) + f" {obj_type}"
     return random.choice(_MISC).format(name=name, obj_type=obj_type,
                                        a_an_thing=a_an_thing)
 
-def name_joke(name, obj_type):
+def name_joke(name, obj_type):  # no-cov
     rnd = random.random()
     if rnd < 0.25:
         joke = _joke1
