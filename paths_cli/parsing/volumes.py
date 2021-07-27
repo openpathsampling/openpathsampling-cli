28d37f7c4293da82dba8f3fb06783b3b16678273
David W.H. Swenson <dwhs@hyperblazer.net>
2021-07-27 07:59:49 -0400

    start using new parameters in volumes

--- paths_cli/parsing/volumes.py
+++ paths_cli/parsing/volumes.py
@@ -1,7 +1,7 @@
 import operator
 import functools
 
-from .core import Parser, InstanceBuilder, custom_eval
+from .core import Parser, InstanceBuilder, custom_eval, Parameter
 from .cvs import cv_parser
 
 class CVVolumeInstanceBuilder(InstanceBuilder):
@@ -23,28 +23,61 @@ def cv_volume_remapper(dct):
 
 build_cv_volume = CVVolumeInstanceBuilder(
     builder=None,
-    attribute_table={
-        'cv': cv_parser,
-        'lambda_min': custom_eval,
-        'lambda_max': custom_eval,
-    },
+    # attribute_table={
+        # 'cv': cv_parser,
+        # 'lambda_min': custom_eval,
+        # 'lambda_max': custom_eval,
+    # },
+    attribute_table=None,
     remapper=cv_volume_remapper,
+    parameters=[
+        Parameter('cv', cv_parser,
+                  description="CV that defines this volume"),
+        Parameter('lambda_min', custom_eval,
+                  description="Lower bound for this volume"),
+        Parameter('lambda_max', custom_eval,
+                  description="Upper bound for this volume")
+    ],
+    name='cv-volume',
+    object_type='volume'
 )
 
 def _use_parser(dct):
     # this is a hack to get around circular definitions
     return volume_parser(dct)
 
+# jsonschema type for combination volumes
+VOL_ARRAY_TYPE = {
+    'type': 'array',
+    'items': {"$ref": "#/definitions/volume_type"}
+}
+
 build_intersection_volume = InstanceBuilder(
     builder=lambda subvolumes: functools.reduce(operator.__and__,
                                                 subvolumes),
-    attribute_table={'subvolumes': _use_parser},
+    # attribute_table={'subvolumes': _use_parser},
+    attribute_table=None,
+    parameters=[
+        Parameter('subvolumes', _use_parser,
+                  json_type=VOL_ARRAY_TYPE,
+                  description="List of the volumes to intersect")
+    ],
+    name='intersection',
+    object_type='volume'
 )
 
 build_union_volume = InstanceBuilder(
     builder=lambda subvolumes: functools.reduce(operator.__or__,
                                                 subvolumes),
-    attribute_table={'subvolumes': _use_parser},
+    # attribute_table={'subvolumes': _use_parser},
+    attribute_table=None,
+    parameters=[
+        Parameter('subvolumes', _use_parser,
+                  json_type=VOL_ARRAY_TYPE,
+                  description="List of the volumes to join into a union")
+    ],
+    name='union',
+    object_type='volume'
 )
 
 TYPE_MAPPING = {
