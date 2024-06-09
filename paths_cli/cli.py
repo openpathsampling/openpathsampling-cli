3867c5b12ac2529f7024d2bc3e79935118ed697e
David W.H. Swenson <dwhs@hyperblazer.net>
2024-06-09 18:40:23 -0500

    Silence annoying pymbar warnings

--- paths_cli/cli.py
+++ paths_cli/cli.py
@@ -94,6 +94,12 @@ def main(log):
         logging.config.fileConfig(log, disable_existing_loggers=False)
     # TODO: if log not given, check for logging.conf in .openpathsampling/
 
+    silence_warnings = ['pymbar.mbar_solvers', 'pymbar.timeseries']
+    for lname in silence_warnings:
+        logger = logging.getLogger(lname)
+        logger.setLevel(logging.CRITICAL)
+
+
     logger = logging.getLogger(__name__)
     logger.debug("About to run command")  # TODO: maybe log invocation?
 
