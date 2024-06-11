1a9c964b81fa581ffc9b114d283604e245b93451
David W.H. Swenson <dwhs@hyperblazer.net>
2024-06-10 20:47:39 -0500

    add TODO to remove when warnings not triggered

--- paths_cli/cli.py
+++ paths_cli/cli.py
@@ -94,6 +94,7 @@ def main(log):
         logging.config.fileConfig(log, disable_existing_loggers=False)
     # TODO: if log not given, check for logging.conf in .openpathsampling/
 
+    # TODO: remove when openmmtools doesn't trigger these warnings
     silence_warnings = ['pymbar.mbar_solvers', 'pymbar.timeseries']
     for lname in silence_warnings:
         logger = logging.getLogger(lname)
